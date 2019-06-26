932e0f116911f83a5c81fa16fc272a91fe3a74b2
miana <miana@claycorp.com>
2019-06-26 16:24:16 -0500

    added game

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class Game
+    {
+
+        private List<Tuple<int, string>> Marks = new List<Tuple<int, string>>();
+        private string CurrentSymbol = "X";
+
+        public void Move(int position)
+        {
+            Marks.Add(Tuple.Create(position, CurrentSymbol));
+            GoToNextTurn();
+        }
+
+        public List<Tuple<int, string>> CurrentMarks()
+        {
+            return Marks;
+        }
+
+        private void GoToNextTurn()
+        {
+            if(CurrentSymbol == "X")
+            {
+                CurrentSymbol = "O";
+            } else
+            {
+                CurrentSymbol = "X";
+            }
+        }
+
+    }
+}
