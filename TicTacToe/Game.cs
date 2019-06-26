12eff48f7cdd4ae65953aae96cfb434d2ba539ab
miana <miana@claycorp.com>
2019-06-26 16:29:28 -0500

    refactored game

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -6,30 +6,23 @@ namespace TicTacToe
 {
     public class Game
     {
-
-        private List<Tuple<int, string>> Marks = new List<Tuple<int, string>>();
         private string CurrentSymbol = "X";
+        private Board board = new Board();
 
         public void Move(int position)
         {
-            Marks.Add(Tuple.Create(position, CurrentSymbol));
+            board.Move(position, CurrentSymbol);
             GoToNextTurn();
         }
 
         public List<Tuple<int, string>> CurrentMarks()
         {
-            return Marks;
+            return board.CurrentMarks();
         }
 
         private void GoToNextTurn()
         {
-            if(CurrentSymbol == "X")
-            {
-                CurrentSymbol = "O";
-            } else
-            {
-                CurrentSymbol = "X";
-            }
+            CurrentSymbol = (CurrentSymbol == "X" ? "O" : "X");
         }
 
     }
