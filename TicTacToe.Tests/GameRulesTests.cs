7a82837b695844c4460484936f7374fbea731a49
Abdulla Mian <miana@claycorp.com>
2019-07-02 16:28:30 -0500

    three in row not optimal

---
+++ TicTacToe.Tests/GameRulesTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe.Tests
+{
+    class GameRulesTests
+    {
+        private Board board;
+        private GameRules rules;
+
+        [SetUp]
+        public void setup()
+        {
+            board = new Board();
+            rules = new GameRules(board);
+        }
+
+        public void gameMoves(int position, string symbol)
+        {
+            board.Move(position, symbol);
+        }
+
+        [Test]
+        public void emptyBoardHasAGameInProgress()
+        {
+            Assert.IsTrue(rules.InProgress());
+        }
+
+        [Test]
+        public void checkIfPlayerHasMadeFirstMove()
+        {
+            board.Move(1, "X");
+            Assert.IsTrue(rules.InProgress());
+        }
+
+        [Test]
+        public void checkIfThereAreThreeMovesInTheHorizontolSpace()
+        {
+            board.Move(1, "X");
+            board.Move(2, "X");
+            board.Move(3, "X");
+            Assert.IsFalse(rules.InProgress());
+        }
+
+        [Test]
+        public void gameIsInProgressIfThereAreTwoMarks()
+        {
+            board.Move(1, "X");
+            board.Move(2, "O");
+            Assert.IsTrue(rules.InProgress());
+        }
+
+        [Test]
+        public void gameIsInProgressIfThereAreFourMarks()
+        {
+            board.Move(1, "X");
+            board.Move(2, "0");
+            board.Move(3, "X");
+            board.Move(6, "0");
+            Assert.IsTrue(rules.InProgress());
+        }
+
+        [Test]
+        public void checkIfThereAreThreeSymbolsInTheHorizontolSpace()
+        {
+            board.Move(1, "O");
+            board.Move(2, "O");
+            board.Move(3, "O");
+            Assert.IsFalse(rules.InProgress());
+        }
+
+        [Test]
+        public void checkIfThereAreThreeSymbolsInTheMiddleHorizontolSpace()
+        {
+            board.Move(4, "O");
+            board.Move(5, "O");
+            board.Move(6, "O");
+            Assert.IsFalse(rules.InProgress());
+        }
+
+    }
+}
---
+++ TicTacToe/GameRules.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class GameRules
+    {
+        private Board board;
+
+        public GameRules(Board gameBoard)
+        {
+            board = gameBoard;
+        }
+
+        public bool InProgress()
+        {
+            return !(threeInARow(1,2,3,"X") || threeInARow(4,5,6,"O") || threeInARow(1,2,3,"O" ));
+        }
+
+        private bool threeInARow(int pos1, int pos2, int pos3, string symbol)
+        {
+            int numberInARow = 0;
+            foreach (Tuple<int, string> mark in board.CurrentMarks())
+            {
+                if (mark.Item1 == pos1 && mark.Item2 == symbol)
+                {
+                    numberInARow += 1;
+                }
+                else if (mark.Item1 == pos2 && mark.Item2 == symbol)
+                {
+                    numberInARow += 1;
+                }
+                else if (mark.Item1 == pos3 && mark.Item2 == symbol)
+                {
+                    numberInARow += 1;
+                }
+
+            }
+            return numberInARow == 3;
+        }
+    }
+}
