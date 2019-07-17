0cad9e21ab71cfd8f832c4e9f6843c831e62bcf3
Abdulla Mian <miana@claycorp.com>
2019-07-17 11:03:07 -0500

    creating board

---
+++ TicTacToe.Tests/BoardPrinterTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe.Tests
+{
+    class BoardPrinterTests
+    {
+        
+        [Test]
+        public void DisplayEmptyBoardIfThereAreNoMarks()
+        {
+            Board board = new Board();
+           
+            BoardPrinter boardPrinter = new BoardPrinter(board);
+
+            Assert.AreEqual("   |   |   \n", boardPrinter.display());
+            
+        }
+
+    }
+}
---
+++ TicTacToe/BoardPrinter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class BoardPrinter
+    {
+       private Board board;
+
+       public BoardPrinter(Board board)
+        {
+            this.board = board;
+        }
+
+        public string display()
+        {
+            return "   |";
+        }
+       
+
+
+    }
+}
